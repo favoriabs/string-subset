7f7e2c5e4391cbf75216cef28545a90a8948cfa0
Favor <favoriabs@gmail.com>
2017-07-06 10:23:11 +0100

    check if percentage value is greater than 100

--- subset.php
+++ subset.php
@@ -1,9 +1,12 @@
 <?php
 //subset function
 
-function subset(string $string, float $percentageValue)
+function subset($string, $percentageValue)
 {
   $lengthOfString = strlen($string);
+  if($percentageValue > 100){
+    $percentageValue = 100;
+  }
   $exactlengthOfPercentValue = (($percentageValue/100) * $lengthOfString);
   $subsetOfString = substr($string, 0, $exactlengthOfPercentValue);
   echo $subsetOfString;
